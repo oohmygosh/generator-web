cd99ed993f74a8fe9b6cde65fabab0156f59dfad
oohmygosh <50185359@qq.com>
2023-01-05 23:20:13 +0800

    添加全局异常捕捉

--- src/generator.ts
+++ src/generator.ts
@@ -1,11 +1,13 @@
 import config from '@/config'
 import * as elIcons from '@element-plus/icons-vue'
+import errorHandler from "@/utils/errorHandler";
 import {App} from "vue";
 
 export default {
     install(app: App) {
         // 挂载全局对象
         app.config.globalProperties.$CONFIG = config
+        app.config.errorHandler = errorHandler
         // 统一注册el-icon图标
         for (const [key, component] of Object.entries(elIcons)) {
             app.component(`ElIcon${key}`, component)
